60a629fc478cafbfe7c75e6c4b6de7f47650f4ad
PavelRahman404 <paveltauker@gmail.com>
2024-10-14 19:54:22 +0600

    Added Create endpoint and view

---
+++ ResortEase.Web/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResortEase.Domain.Entities;
+using ResortEase.Infrastructure.Data;
+
+namespace ResortEase.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
