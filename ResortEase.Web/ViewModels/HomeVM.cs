6d0ea0a4a2566335967d507274d50018c94d2725
PavelRahman404 <paveltauker@gmail.com>
2024-10-19 19:51:26 +0600

    Home Page UI , Home Controller and Modal

---
+++ ResortEase.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using ResortEase.Domain.Entities;
+
+namespace ResortEase.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
